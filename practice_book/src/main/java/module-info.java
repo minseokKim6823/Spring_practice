31d34ea51e45b7e59a217941a51d713c2b35c182
minseokKim6823 <alstjr6823@naver.com>
2023-07-19 01:49:27 +0900

    .

--- practice_book/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.practice_book {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.practice_book to javafx.fxml;
-    exports com.example.practice_book;
-}
